2b83e6b1cc45f35494bc11e7104cbcc0186b5eb1
nathaliejanep <nathalie.persson@hotmail.com>
2024-01-31 15:43:39 +0100

    Implement tests and update rootdir to './'

---
+++ __tests__/helpers.test.ts
@@ -0,0 +1,26 @@
+import { constructPath, convertForm } from '../src/utils/helpers';
+
+describe('constructPath function', () => {
+  test('should return correct path for "index.html', () => {
+    const relativePath = 'index.html';
+    const result = constructPath(relativePath);
+    expect(result).toEqual(`/${relativePath}`);
+  });
+});
+
+test('should return correct path for other files', () => {
+  const relativePath = 'dashboard.html';
+  const result = constructPath(relativePath);
+  expect(result).toEqual(`/src/pages/${relativePath}`);
+});
+
+describe('convertForm function', () => {
+  test('should convert FormData to object', () => {
+    const formData = new FormData();
+    formData.append('name', 'Jane Doe');
+    formData.append('email', 'student@example.com');
+
+    const result = convertForm<{ name: string; email: string }>(formData);
+    expect(result).toEqual({ name: 'Jane Doe', email: 'student@example.com' });
+  });
+});
